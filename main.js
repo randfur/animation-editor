7692aa7237b34061f0cbb91f4885e6c1b942439a
Alan Cutter <alancutter@chromium.org>
2022-12-16 14:18:44 +1100

    no render

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-import {runUi, Ui} from './ui.js';
+import {Ui, createElement} from './ui.js';
+
+class UiButton extends Ui {
+  static tag = 'ui-button';
+  init({text, onClick}) {
+    this.button = createElement({
+      tag: 'button',
+      text,
+    });
+    this.button.addEventListener('click', onClick);
+
+    this.append(this.button);
+  }
+}
+
+class App extends Ui {
+  static tag = 'test-bark';
+  init() {
+    this.text = createElement({text: 'dog'});
+    this.uiButton = UiButton.create({
+      text: 'click me',
+      onClick: () => this.text.textContent = 'bark',
+    });
+
+    this.append(
+      this.text,
+      this.uiButton,
+    );
+  }
+}
 
 async function main() {
-  runUi(class extends Ui {
-    static tag = 'test-bark';
-    init() {
-      this.render([
-        {text: 'dogs'},
-      ]);
-    }
-  });
+  document.body.appendChild(App.create());
 }
 
 main();
--- ui.js
+++ ui.js
@@ -1,95 +1,25 @@
-function inheritsFrom(TypeA, TypeB) {
-  return TypeA === TypeB || TypeA.prototype instanceof TypeB;
-}
-
-class CreateUi {
-  constructor(uiType, initParams={}) {
-    console.assert(inheritsFrom(uiType, Ui));
-    this.uiType = uiType;
-    this.initParams = initParams;
-  }
-}
-
-function appendFlattened(destination, element, isContainer) {
-  if (isContainer) {
-    for (const child of element.children) {
-      destination.appendChild(child);
-    }
-  } else {
-    destination.appendChild(element);
-  }
-}
-
-function renderTemplate(template, refs) {
-  if (template === null) {
-    const element = document.createElement('div');
-    element.style.display = 'none';
-    return {
-      element,
-      isContainer: false,
-    };
-  }
-
-  if (typeof template === 'string') {
-    const element = document.createElement('div');
-    element.textContent = template;
-    return {
-      element,
-      isContainer: false,
-    };
-  }
-
-  if (template instanceof CreateUi) {
-    const {uiType, initParams} = template;
-    if (!customElements.get(uiType.tag)) {
-      customElements.define(uiType.tag, uiType);
-    }
-    const element = document.createElement(uiType.tag);
-    element.init(initParams);
-    return {
-      element,
-      isContainer: false,
-    };
-  }
-
-  if (template instanceof Array) {
-    const container = document.createElement('div');
-    for (const item of template) {
-      const {element, isContainer} = renderTemplate(item, refs);
-      appendFlattened(container, element, isContainer);
-    }
-    return {
-      element: container,
-      isContainer: true,
-    };
-  }
-
-  console.assert(template instanceof Object);
-  const container = document.createElement('div');
-  for (const [key, value] of Object.entries(template)) {
-    const {element} = renderTemplate(value, refs);
-    element.id = key;
-    refs[key] = element;
-    container.appendChild(element);
-  }
-  return {
-    element: container,
-    isContainer: true,
-  };
-}
+const registered = new Set();
 
 export class Ui extends HTMLElement {
-  render(template) {
-    this.refs = {};
-    const {element, isContainer} = renderTemplate(template, this.refs);
-    appendFlattened(this, element, isContainer);
+  static tag = null;
+
+  static create(initParams) {
+    if (!registered.has(this)) {
+      console.assert(!customElements.get(this.tag));
+      customElements.define(this.tag, this);
+      registered.add(this);
+    }
+    const instance = document.createElement(this.tag);
+    instance.init(initParams);
+    return instance;
   }
 }
 
-export function createUi(uiType, initParams) {
-  return new CreateUi(uiType, initParams);
-}
-
-export function runUi(uiType, initParams) {
-  document.body.appendChild(renderTemplate(createUi(uiType, initParams), {}).element);
+export function createElement({tag='div', text='', children}) {
+  const element = document.createElement(tag);
+  element.textContent = text;
+  if (children) {
+    element.append(...children);
+  }
+  return element;
 }
